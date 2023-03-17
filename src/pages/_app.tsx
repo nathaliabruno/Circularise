ee51968c7dea87e370b1fd1b4089a0c1f8967752
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-17 13:20:50 -0300

    Setup cypress and react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useState } from "react"
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
