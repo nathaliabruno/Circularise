b800c0f4ea90a62a10ba2889fd7f23624cb36246
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-17 22:04:51 -0300

    Set Org Interface

---
+++ src/components/Organization.tsx
@@ -0,0 +1,21 @@
+import { Card } from "./Card"
+
+interface IOrg {
+  login: string
+  id: number
+  node_id: string
+  url: string
+  repos_url: string
+  events_url: string
+  hooks_url: string
+  issues_url: string
+  members_url: string
+  public_members_url: string
+  avatar_url: string
+  description?: string
+}
+const Organization = (props: IOrg) => {
+  return <Card>Org</Card>
+}
+
+export default Organization
