4b71b1224f2a9656c4776d48a868331749142d7c
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-17 22:03:34 -0300

    Add svg support

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,19 @@ module.exports = {
     extend: {
       colors: {
         black: "#050505",
-        grey500: "#8c8c8c",
-        grey400: "#bdbdbd",
-        grey300: "#e0e0e0",
-        grey200: "#f2f2f2",
-        grey100: "#f9f9f9",
+        gray: {
+          500: "#8c8c8c",
+          400: "#bdbdbd",
+          300: "#e0e0e0",
+          200: "#f2f2f2",
+          100: "#f9f9f9",
+        },
         blue: "#2f80ed",
         blue8: "#2f80ed08",
       },
       fontSize: { base: "1rem", lg: "1.125rem", xl: "1.5rem", "2xl": "2rem" },
       fontFamily: { karla: "Karla" },
-      boxShadow: { dropshadow: "2px 4px 16px 0px rgba(196,196,196,0.3)" },
+      boxShadow: { card: "2px 4px 16px 0px rgba(196,196,196,0.3)" },
       borderRadius: {
         none: "0",
         xs: "0.3125rem",
