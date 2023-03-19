c0d3090464aae73ba419e05874c797d51c882eab
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-19 14:14:31 -0300

    add font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image"
-import { Inter } from "next/font/google"
 import PageContainer from "../components/PageContainer"
-const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-karla">
       <PageContainer />
     </main>
   )
