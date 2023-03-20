17e98b79fe1aa9f12a1a53574315b683890acc85
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-20 11:31:15 -0300

    Add margin to page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <main className="p-24 font-karla">
       <div className="bg-gray-100 rounded text-black pt-16 pb-16 pr-20 pl-20 w-8/12 m-auto">
-        <Text tag="h1">GitHub organizations and their members</Text>
+        <Text tag="h1" styling="mb-6">
+          GitHub organizations and their members
+        </Text>
         <OrganizationsList />
       </div>
     </main>
