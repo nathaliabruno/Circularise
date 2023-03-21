35182e6030ed44aabb8964abe8d1d97d13dc1bed
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-21 15:29:57 -0300

    Add .env file to process api token

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,26 +1,29 @@
+const API_TOKEN = process.env.GH_TOKEN
+
 const fetchOrgs = async () => {
-  const organizations = await fetch(
-    "https://api.github.com/organizations?per_page=6&since=4242",
-    {
-      headers: new Headers({
-        Authorization: "Bearer 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",
-      }),
-    }
-  )
+  const organizations = API_TOKEN
+    ? await fetch(
+        'https://api.github.com/organizations?per_page=6&since=4242',
+        {
+          headers: new Headers({
+            Authorization: `Bearer ${API_TOKEN}`,
+          }),
+        }
+      )
+    : await fetch('https://api.github.com/organizations?per_page=6&since=4242')
   return organizations.json()
 }
 
 const fetchMembers = async (orgLogin: string) => {
   if (!orgLogin) return
 
-  const members = await fetch(
-    `https://api.github.com/orgs/${orgLogin}/public_members`,
-    {
-      headers: new Headers({
-        Authorization: "Bearer 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",
-      }),
-    }
-  )
+  const members = API_TOKEN
+    ? await fetch(`https://api.github.com/orgs/${orgLogin}/public_members`, {
+        headers: new Headers({
+          Authorization: `Bearer ${API_TOKEN}`,
+        }),
+      })
+    : await fetch(`https://api.github.com/orgs/${orgLogin}/public_members`)
   return members.json()
 }
 
